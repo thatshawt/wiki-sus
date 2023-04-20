a671e822598d2f20282826d389207b46b6e72e08
Salomon Aduen <salimaduen@techexchange.in>
2023-04-20 00:07:24 +0000

    Added timestamps to messages

--- flaskr/message.py
+++ flaskr/message.py
@@ -1,7 +1,10 @@
+from datetime import date, datetime
 class Message:
     def __init__(self, author, message):
         self.author = author
         self.message = message
+        self.date = date.today()
+        self.time = datetime.now().strftime("%H:%M")
 
     def get_author(self):
         return self.author
